0318d65e2c1bd6ee28ad03d9dcc6bb3d85d72e7f
russoanto <russoantonio451@gmail.com>
2023-10-26 09:17:45 +0200

    first commit

---
+++ build/CMakeFiles/tracker.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tracker.dir/src/CloudManager.cpp.o"
+  "CMakeFiles/tracker.dir/src/tracker/KalmanFilter.cpp.o"
+  "CMakeFiles/tracker.dir/src/tracker/Tracker.cpp.o"
+  "CMakeFiles/tracker.dir/src/tracker/Tracklet.cpp.o"
+  "CMakeFiles/tracker.dir/src/viewer/Renderer.cpp.o"
+  "libtracker.pdb"
+  "libtracker.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
